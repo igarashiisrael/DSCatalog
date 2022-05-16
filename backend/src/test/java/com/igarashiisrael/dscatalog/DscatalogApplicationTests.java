c2502cdc6d2e0d1b9601967661001050115716fd
Israel <igarashiisrael@icloud.com>
2022-05-16 17:23:51 -0300

    Backend project created

---
+++ backend/src/test/java/com/igarashiisrael/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igarashiisrael.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
