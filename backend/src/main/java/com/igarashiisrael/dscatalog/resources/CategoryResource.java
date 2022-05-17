a4dac367c84930a8b974b0bde676f0d6edaf2e1c
Israel <igarashiisrael@icloud.com>
2022-05-17 10:59:11 -0300

    DTO

--- backend/src/main/java/com/igarashiisrael/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/igarashiisrael/dscatalog/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.igarashiisrael.dscatalog.resources;
 
+import com.igarashiisrael.dscatalog.dto.CategoryDTO;
 import com.igarashiisrael.dscatalog.entities.Category;
 import com.igarashiisrael.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,8 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll(){
+        List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
--- backend/src/main/java/com/igarashiisrael/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/igarashiisrael/dscatalog/services/CategoryService.java
@@ -1,12 +1,15 @@
 package com.igarashiisrael.dscatalog.services;
 
+import com.igarashiisrael.dscatalog.dto.CategoryDTO;
 import com.igarashiisrael.dscatalog.entities.Category;
 import com.igarashiisrael.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -15,7 +18,10 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll() {
-        return repository.findAll();
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+
+        return list.stream().map(x-> new CategoryDTO(x)).collect(Collectors.toList());
+
     }
 }
