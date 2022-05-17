100778e56adf58beac9c03718dd789235927ec5f
Israel <igarashiisrael@icloud.com>
2022-05-17 08:59:15 -0300

    Database seeding

--- backend/src/main/java/com/igarashiisrael/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/igarashiisrael/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.igarashiisrael.dscatalog.entities.Category;
 import com.igarashiisrael.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return repository.findAll();
     }
