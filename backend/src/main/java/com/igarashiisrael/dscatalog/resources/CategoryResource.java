b9928e59b9d01bd52c87610db4dc41eddad539da
Israel <igarashiisrael@icloud.com>
2022-05-21 11:46:51 -0300

    POST Category

--- backend/src/main/java/com/igarashiisrael/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/igarashiisrael/dscatalog/resources/CategoryResource.java
@@ -5,11 +5,10 @@ import com.igarashiisrael.dscatalog.entities.Category;
 import com.igarashiisrael.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +29,14 @@ public class CategoryResource {
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
         CategoryDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
