1573e1d8f45e9b8c2b101011f0fd8349141216b8
Israel <igarashiisrael@icloud.com>
2022-05-22 10:26:33 -0300

    Pagination

--- backend/src/main/java/com/igarashiisrael/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/igarashiisrael/dscatalog/resources/CategoryResource.java
@@ -4,6 +4,9 @@ import com.igarashiisrael.dscatalog.dto.CategoryDTO;
 import com.igarashiisrael.dscatalog.entities.Category;
 import com.igarashiisrael.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -20,8 +23,16 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll(){
-        List<CategoryDTO> list = service.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        Page<CategoryDTO> list = service.findAllPaged(pageRequest);
+
         return ResponseEntity.ok().body(list);
     }
 
--- backend/src/main/java/com/igarashiisrael/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/igarashiisrael/dscatalog/services/CategoryService.java
@@ -8,6 +8,8 @@ import com.igarashiisrael.dscatalog.services.exceptions.ResourceNotFoundExceptio
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,10 +25,11 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll() {
-        List<Category> list = repository.findAll();
+    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+        Page<Category> list = repository.findAll(pageRequest);
 
-        return list.stream().map(x-> new CategoryDTO(x)).collect(Collectors.toList());
+
+        return list.map(x-> new CategoryDTO(x));
     }
 
     @Transactional(readOnly = true)
