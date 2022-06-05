3442c73786af07111c5c71c584f73d2b8dada13f
Israel <igarashiisrael@icloud.com>
2022-06-04 21:10:09 -0300

    Refactor pagleagle

--- backend/src/main/java/com/igarashiisrael/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/igarashiisrael/dscatalog/resources/ProductResource.java
@@ -1,15 +1,12 @@
 package com.igarashiisrael.dscatalog.resources;
-
 import com.igarashiisrael.dscatalog.dto.ProductDTO;
 import com.igarashiisrael.dscatalog.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import java.net.URI;
 
 @RestController
@@ -20,16 +17,8 @@ public class ProductResource {
     private ProductService service;
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-    ){
-        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
-
-        Page<ProductDTO> list = service.findAllPaged(pageRequest);
-
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
+        Page<ProductDTO> list = service.findAllPaged(pageable);
         return ResponseEntity.ok().body(list);
     }
 
