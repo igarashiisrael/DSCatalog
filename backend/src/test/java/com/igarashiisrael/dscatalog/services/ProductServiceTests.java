3dd2f0b5a559492265cb2dd3a61cba492de6d896
Israel <igarashiisrael@icloud.com>
2022-07-04 15:50:35 -0300

    delete Should Throw Resource Not Found Exception When Id Not Exists

--- backend/src/test/java/com/igarashiisrael/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/igarashiisrael/dscatalog/services/ProductServiceTests.java
@@ -1,10 +1,15 @@
 package com.igarashiisrael.dscatalog.services;
 
 import com.igarashiisrael.dscatalog.repositories.ProductRepository;
+import com.igarashiisrael.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.Mockito;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -15,6 +20,33 @@ public class ProductServiceTests {
 
     @Mock
     private ProductRepository repository;
+
+    private long existingId;
+    private long NonExistingId;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        existingId = 1L;
+        NonExistingId = 1000L;
+        Mockito.doNothing().when(repository).deleteById(existingId);
+        Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(NonExistingId);
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdNotExists(){
+        Assertions.assertThrows(ResourceNotFoundException.class, ()->{
+            service.delete(NonExistingId);
+        });
+        Mockito.verify(repository, Mockito.times(1)).deleteById(NonExistingId);
+    }
+
+    @Test
+    public void deleteShouldDoNothingWhenIdExists(){
+        Assertions.assertDoesNotThrow(() -> {
+            service.delete((existingId));
+        });
+        Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+    }
 
 
 }
