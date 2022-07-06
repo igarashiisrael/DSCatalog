c6557cb5728f63a183d930527fbf6c6a84106ef6
Israel <igarashiisrael@icloud.com>
2022-07-06 16:09:13 -0300

    startingTestsInTheWebLayer

---
+++ backend/src/test/java/com/igarashiisrael/dscatalog/resources/ProductResourceTests.java
@@ -0,0 +1,57 @@
+package com.igarashiisrael.dscatalog.resources;
+
+import com.igarashiisrael.dscatalog.dto.ProductDTO;
+import com.igarashiisrael.dscatalog.services.ProductService;
+import com.igarashiisrael.dscatalog.tests.Factory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductResource.class)
+public class ProductResourceTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService service;
+
+    private ProductDTO productDTO;
+    private PageImpl<ProductDTO> page;
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+        productDTO = Factory.createProductDTO();
+
+        page = new PageImpl<>(List.of(productDTO));
+
+        when(service.findAllPaged(any())).thenReturn(page);
+    }
+
+    @Test
+    public void findAllShouldReturnPage() throws Exception {
+
+        ResultActions result =
+                mockMvc.perform(get("/products")
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk());
+
+        //mockMvc.perform(get("/products")).andExpect(status().isOk());
+    }
+
+}
--- backend/src/test/java/com/igarashiisrael/dscatalog/tests/Factory.java
+++ backend/src/test/java/com/igarashiisrael/dscatalog/tests/Factory.java
@@ -10,7 +10,7 @@ public class Factory {
 
     public static Product createProduct(){
         Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
-        product.getCategories().add(new Category(2L, "Eletronics"));
+        product.getCategories().add(new Category(1L, "Eletronics"));
         return product;
     }
 
