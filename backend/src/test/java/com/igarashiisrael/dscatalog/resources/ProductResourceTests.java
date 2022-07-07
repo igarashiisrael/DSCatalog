543d9c2c75e61d10d60bf72a0834adcabd9a53bb
Israel <igarashiisrael@icloud.com>
2022-07-07 01:04:55 -0300

    IntegrationTestsInTheWebLayer

--- backend/src/test/java/com/igarashiisrael/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/igarashiisrael/dscatalog/resources/ProductResourceTests.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
